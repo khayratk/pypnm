91b05dccfbca0e6389edfdc45273e7b1cf075adc
kkhayrat <khayratk@ifd.mavt.ethz.ch>
2018-01-12 20:43:30 +0100

    Tensorial absolute permeability

--- pypnm/porenetwork/relperm.py
+++ pypnm/porenetwork/relperm.py
@@ -1,62 +1,29 @@
 import numpy as np
-
+from pypnm.linalg.laplacianmatrix import laplacian_from_network
 from fluxes import flux_into_pores
 from pypnm.attribute_calculators.conductance_calc import ConductanceCalc
-from pypnm.linalg.linear_system_solver import LinearSystemStandard
 from pypnm.percolation import graph_algs
 from pypnm.porenetwork.constants import *
-try:
-    from sim_settings import *
-except ImportError:
-    sim_settings = dict()
-    sim_settings["fluid_properties"] = dict()
-    sim_settings["fluid_properties"]['mu_n'] = 1.0
-    sim_settings["fluid_properties"]['mu_w'] = 1.0
-    sim_settings["fluid_properties"]['gamma'] = 1.0
+from pypnm.linalg.petsc_interface import petsc_solve
 
 
-class _PressureSolver(object):
-    def __init__(self, network):
+class SimpleRelPermComputer(object):
+    def __init__(self, network, fluid_properties, pores_have_conductance=False):
         self.network = network
-        self.ls = LinearSystemStandard(network)
-        self.sol = None
-
-    def solve_pressure(self, FLAG, pi_in, pi_out, mask_ignored_tubes=None):
-        """ Solves for pressure over a specific field over nodes of pore-network
-        Args:
-            FLAG: NWETT or WETT
-        """
-        network = self.network
-
-        if mask_ignored_tubes is None:
-            mask_ignored_tubes = np.zeros(self.network.nr_t, dtype=np.bool)
-
-        if FLAG == NWETT:
-            conductance = np.copy(network.tubes.k_n)
-            conductance[mask_ignored_tubes] = 0.0
-
-        if FLAG == WETT:
-            conductance = np.copy(network.tubes.k_w)
-            conductance[mask_ignored_tubes] = 0.0
-
-        self.ls.fill_matrix(conductance)
-        self.ls.set_dirichlet_pores(pi_in, 1.0)
-        self.ls.set_dirichlet_pores(pi_out, 0.0)
-        self.sol = self.ls.solve("AMG")
-
-
-class RelPermComputer(object):
-    def __init__(self, network):
-        self.network = network
-
-        self.ls = _PressureSolver(self.network)
-
         self.total_flux_one_phase = None
         self.kr_n = np.zeros(3)
         self.kr_w = np.zeros(3)
-
-        self.K = 1.0e-8
+        self.K = 1.0e-8 * np.ones(3)
         self.is_isotropic = True
+
+        # Mask of tubes and pores not connected to the inlet, which makes the matrix singular
+        p_conn_inlet, t_conn_inlet = graph_algs.get_pore_and_tube_connectivity_to_pore_list(self.network, self.network.pi_list_face[WEST])
+        self.mask_ignored_tubes = (t_conn_inlet == 0)
+
+        self.fluid_properties = fluid_properties
+
+        self.cond_computer = ConductanceCalc(self.network, fluid_properties, pores_have_conductance)
+        self.compute_absolute_permeability()
 
     def compute_isotropic(self):
         kr_n, kr_w = self.compute_relperms(self.ls, self.network.pi_list_face[WEST], self.network.pi_list_face[EAST])
@@ -74,35 +41,39 @@ class RelPermComputer(object):
         assert np.all(self.kr_n <= 18.0)
         assert np.all(self.kr_w <= 18.0)
 
-    def compute(self):
-        raise NotImplementedError("Method not implemented")
 
+    def _general_absolute_permeability(self, coord, pi_inlet, pi_outlet):
+        network = self.network
+        tube_k_w = self.cond_computer.conductances_fully_wetting()
 
-class SimpleRelPermComputer(RelPermComputer):
-    def __init__(self, network):
-        super(SimpleRelPermComputer, self).__init__(network)
+        pi_dirichlet = np.union1d(pi_inlet, pi_outlet)
+        A = laplacian_from_network(network, weights=tube_k_w, ind_dirichlet=pi_dirichlet)
+        rhs = np.zeros(network.nr_p)
+        rhs[pi_inlet] = 1.0
 
-        # Mask of tubes and pores not connected to the inlet, which makes the matrix singular
-        p_conn_inlet, t_conn_inlet = graph_algs.get_pore_and_tube_connectivity_to_pore_list(self.network, self.network.pi_list_face[WEST])
-        self.mask_ignored_tubes = (t_conn_inlet == 0)
+        pressure = petsc_solve(A*1e20, rhs*1e20, tol=1e-12)
 
-        self.cond_computer = ConductanceCalc(self.network)
-        self.compute_absolute_permeability()
+        mu_w = self.fluid_properties["mu_w"]
+        self.total_flux_one_phase = flux_into_pores(network, pressure, tube_k_w, pi_inlet)
+
+        x_max = np.mean(coord[pi_outlet])
+        x_min = np.mean(coord[pi_inlet])
+
+        return mu_w * np.abs(self.total_flux_one_phase) * (x_max-x_min)
 
     def compute_absolute_permeability(self):
-        self.cond_computer.compute_fully_wetting()
-        ls = _PressureSolver(self.network)
-        ls.solve_pressure(FLAG=WETT, pi_in=self.network.pi_list_face[WEST], pi_out=self.network.pi_list_face[EAST], mask_ignored_tubes=self.mask_ignored_tubes)
-        self.total_flux_one_phase = flux_into_pores(self.network, ls.sol, self.network.tubes.k_w, self.network.pi_list_face[WEST])
+        network = self.network
 
-        assert np.all(self.network.tubes.k_w > 0.0)
-        assert np.all(self.network.tubes.k_w < 0.1)
+        self.K[0] = self._general_absolute_permeability(network.pores.x,
+                                                        network.pi_list_face[WEST], network.pi_list_face[EAST])
 
-        mu_w = sim_settings['fluid_properties']['mu_w']
+        self.K[1] = self._general_absolute_permeability(network.pores.y,
+                                                        network.pi_list_face[SOUTH], network.pi_list_face[NORTH])
 
-        # TODO: This is only generally valid for CUBIC or SQUARE isotropic pore-networks!
-        self.K = mu_w*np.abs(self.total_flux_one_phase) * (np.max(self.network.pores.x) - np.min(self.network.pores.x))
-        assert self.K > 0.0
+        self.K[2] = self._general_absolute_permeability(network.pores.z,
+                                                        network.pi_list_face[BOTTOM], network.pi_list_face[TOP])
+
+        assert np.all(self.K > 0.0), self.K
 
     def compute_relperms(self, ls, pi_in, pi_out):
         network = self.network
@@ -127,8 +98,8 @@ class SimpleRelPermComputer(RelPermComputer):
 
                 total_flux_nwett = flux_into_pores(self.network, ls.sol, self.network.tubes.k_n, pi_in)
 
-                mu_n = sim_settings['fluid_properties']['mu_n']
-                mu_w = sim_settings['fluid_properties']['mu_w']
+                mu_n = self.fluid_properties['mu_n']
+                mu_w = self.fluid_properties['mu_w']
                 kr_n = total_flux_nwett / self.total_flux_one_phase * mu_n / mu_w
 
         if kr_n < -10.0e-9:
@@ -146,7 +117,4 @@ class SimpleRelPermComputer(RelPermComputer):
         return kr_n, kr_w
 
     def compute(self):
-        if self.is_isotropic:
-            self.compute_isotropic()
-        else:
-            self.compute_anisotropic()
+        self.compute_isotropic()
