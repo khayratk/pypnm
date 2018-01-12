f043efb8240f2b03a186e714aeffa5b1feafaef1
kkhayrat <khayratk@ifd.mavt.ethz.ch>
2018-01-12 20:51:33 +0100

    added support functions for msrsb. to be finalized later

--- pypnm/ams/msrb_funcs.py
+++ pypnm/ams/msrb_funcs.py
@@ -87,6 +87,78 @@ def solve_with_msrsb(ia, ja, a, rhs, tol=1e-3, v_per_subdomain=1000):
     unique_map, nonunique_map, subgraph_ids_vec = MultiScaleSimUnstructured.create_maps(graph, comm)
 
     epetra_importer = Epetra.Import(nonunique_map, unique_map)
+    assert epetra_importer.NumPermuteIDs() == 0
+    assert epetra_importer.NumSameIDs() == unique_map.NumMyElements()
+
+    my_basis_support = dict()
+
+    my_global_elements = unique_map.MyGlobalElements()
+
+    graph["global_to_local"] = dict((v["global_id"], v.index) for v in graph.vs)
+    graph["local_to_global"] = dict((v.index, v["global_id"]) for v in graph.vs)
+
+    my_restriction_supports = dict()
+
+    subgraph_id_vec = np.asarray(graph.vs['subgraph_id'])
+    for i in subgraph_ids:
+        vs_subgraph = (subgraph_id_vec == i).nonzero()[0]
+        my_restriction_supports[i] = graph.vs.select(vs_subgraph)["global_id"]
+
+    for i in subgraph_ids:
+        support_vertices = support_of_basis_function(i, graph, coarse_graph, subgraph_id_to_v_center_id,
+                                                     my_restriction_supports)
+
+        my_basis_support[i] = np.intersect1d(support_vertices, my_global_elements).astype(np.int32)
+
+    ms = MSRSB(A, my_restriction_supports, my_basis_support)
+    sol = np.asarray(solve_multiscale(ms, A, vector_numpy_to_epetra(rhs), tol=tol))
+    return sol, ms, subgraph_id_to_v_center_id
+
+
+def get_supports(A_scipy, v_per_subdomain=1000):
+    comm = Epetra.PyComm()
+    num_proc = comm.NumProc()
+
+    graph = scipy_matrix_to_igraph(A_scipy)
+
+    num_subdomains = A_scipy.get_shape()[0] / v_per_subdomain
+
+
+    # create global_id attributes before creating subgraphs.
+    graph.vs["global_id"] = np.arange(graph.vcount())
+    graph.es["global_id"] = np.arange(graph.ecount())
+
+    n_components, _ = connected_components(A_scipy, directed=False)
+    print "number of components", n_components
+
+    if n_components == 1:
+        _, subgraph_ids_each_vertex = pymetis.part_graph(num_subdomains, graph.get_adjlist())
+    else:
+        _, subgraph_ids_each_vertex = partition_multicomponent_graph(A_scipy, v_per_subdomain)
+        print "num of subdomains created", _
+
+    subgraph_ids_each_vertex = np.asarray(subgraph_ids_each_vertex)
+    graph.vs["subgraph_id"] = subgraph_ids_each_vertex
+
+    # Assign a processor id to each subgraph
+    coarse_graph = coarse_graph_from_partition(graph, subgraph_ids_each_vertex)
+
+    _, proc_ids = pymetis.part_graph(num_proc, coarse_graph.get_adjlist())
+    coarse_graph.vs['proc_id'] = proc_ids
+    coarse_graph.vs["subgraph_id"] = np.arange(coarse_graph.vcount())
+
+    # Assign a processor id to each pore
+    subgraph_id_to_proc_id = {v["subgraph_id"]: v['proc_id'] for v in coarse_graph.vs}
+    graph.vs["proc_id"] = [subgraph_id_to_proc_id[v["subgraph_id"]] for v in graph.vs]
+
+    subgraph_ids = np.unique(subgraph_ids_each_vertex)
+
+    subgraph_id_to_v_center_id = MultiScaleSimUnstructured.subgraph_central_vertices(graph, subgraph_ids)
+
+    # Epetra maps to facilitate data transfer between processors
+    unique_map, nonunique_map, subgraph_ids_vec = MultiScaleSimUnstructured.create_maps(graph, comm)
+
+    epetra_importer = Epetra.Import(nonunique_map, unique_map)
     epetra_importer.NumPermuteIDs() == 0
     epetra_importer.NumSameIDs() == unique_map.NumMyElements()
 
@@ -110,5 +182,4 @@ def solve_with_msrsb(ia, ja, a, rhs, tol=1e-3, v_per_subdomain=1000):
 
         my_basis_support[i] = np.intersect1d(support_vertices, my_global_elements).astype(np.int32)
 
-    ms = MSRSB(A, my_restriction_supports, my_basis_support)
-    return np.asarray(solve_multiscale(ms, A, vector_numpy_to_epetra(rhs), tol=tol)), ms, subgraph_id_to_v_center_id
+    return my_restriction_supports, my_basis_support
