f7f45ff94bf78c8aa19d5b1ef43064f3e3613539
kkhayrat <khayratk@ifd.mavt.ethz.ch>
2018-01-12 20:49:22 +0100

    Added msrsb two-phase test

--- pypnm/ams/test/test_msrsb.py
+++ pypnm/ams/test/test_msrsb.py
@@ -6,14 +6,66 @@ import numpy as np
 from pypnm.ams.msrb_funcs import solve_with_msrsb
 from pypnm.attribute_calculators.conductance_calc import ConductanceCalc
 from pypnm.linalg.laplacianmatrix import LaplacianMatrix
-from pypnm.linalg.linear_system_solver import solve_pyamg
 from pypnm.porenetwork.network_factory import structured_network
-from pypnm.porenetwork.network_factory import unstructured_network
+from pypnm.porenetwork.network_factory import unstructured_network_delaunay
+from pypnm.util.utils_testing import run_ip_algorithm
+from scipy.sparse.linalg import spsolve
+
+sim_settings = dict()
+sim_settings["fluid_properties"] = dict()
+sim_settings["fluid_properties"]['mu_n'] = 0.001
+sim_settings["fluid_properties"]['mu_w'] = 1.0
+sim_settings["fluid_properties"]['gamma'] = 1.0
+
+
+def test_msrsb_two_phase():
+    nx, ny, nz = 3, 7, 11
+    n_fine_per_cell = 5
+    network = structured_network(Nx=n_fine_per_cell * nx, Ny=n_fine_per_cell * ny, Nz=n_fine_per_cell * nz)
+    run_ip_algorithm(network, 0.7)
+    p_c = network.pores.p_c
+    assert np.sum(p_c) > 0.0
+
+    k_computer = ConductanceCalc(network, sim_settings["fluid_properties"], pores_have_conductance=True)
+    k_computer.compute()
+
+    assert np.sum(network.tubes.k_n) > 0.0
+
+    A = LaplacianMatrix(network)
+    A.set_edge_weights(network.tubes.k_w + network.tubes.k_n)
+    A = A.get_csr_matrix()
+
+    An = LaplacianMatrix(network)
+    An.set_edge_weights(network.tubes.k_n)
+    An = An.get_csr_matrix()
+
+    k_tot = network.tubes.k_n+network.tubes.k_w
+    print "max/min total conductance ratio is ", max(k_tot)/min(k_tot)
+
+    rhs = np.zeros(network.nr_p)
+    rhs[0] = 1e-10
+    rhs[100] = -1e-10
+    rhs += -An * p_c
+
+    sol, _, _ = solve_with_msrsb(A.indices, A.indptr, A.data, rhs, tol=1e-12)
+
+    A[1, :] = 0.0
+    A[1, 1] = 1.0
+
+    sol_exact = spsolve(A=A, b=rhs)
+
+    sol -= np.min(sol)
+    sol_exact -= np.min(sol_exact)
+    error = sol - sol_exact
+
+    Linf = np.linalg.norm(error, ord=np.inf) / np.linalg.norm(sol_exact, ord=np.inf)
+    L2_error = np.linalg.norm(error, ord=2) / np.linalg.norm(sol_exact, ord=2)
+    assert ((Linf < 10e-2) & (L2_error < 10e-5)), "L1 error: %s  L2 error: %s" % (Linf, L2_error)
 
 
 def test_msrsb_unstructured():
-    network = unstructured_network(nr_pores=10000)
-    k_computer = ConductanceCalc(network)
+    network = unstructured_network_delaunay(nr_pores=10000)
+    k_computer = ConductanceCalc(network, sim_settings["fluid_properties"], pores_have_conductance=True)
     k_computer.compute()
 
     A = LaplacianMatrix(network)
@@ -22,14 +74,14 @@ def test_msrsb_unstructured():
     rhs = np.zeros(network.nr_p)
     rhs[0] = 1e-10
     rhs[100] = -1e-10
-    sol = solve_with_msrsb(A.indices, A.indptr, A.data, rhs)
+    sol, _, _ = solve_with_msrsb(A.indices, A.indptr, A.data, rhs, tol=1e-12)
 
     A[1, :] = 0.0
     A[1, 1] = 1.0
-    sol_exact = solve_pyamg(A, rhs, tol=1e-10)
+    sol_exact = spsolve(A=A, b=rhs)
 
-    sol = (sol - np.min(sol))
-    sol_exact = (sol_exact-np.min(sol_exact))
+    sol -= np.min(sol)
+    sol_exact -= np.min(sol_exact)
     error = sol - sol_exact
 
     Linf = np.linalg.norm(error, ord=np.inf) / np.linalg.norm(sol_exact, ord=np.inf)
@@ -41,7 +93,7 @@ def test_msrsb_structured():
     nx, ny, nz = 3, 7, 11
     n_fine_per_cell = 5
     network = structured_network(Nx=n_fine_per_cell * nx, Ny=n_fine_per_cell * ny, Nz=n_fine_per_cell * nz)
-    k_computer = ConductanceCalc(network)
+    k_computer = ConductanceCalc(network, sim_settings["fluid_properties"], pores_have_conductance=True)
     k_computer.compute()
 
     A = LaplacianMatrix(network)
@@ -50,14 +102,14 @@ def test_msrsb_structured():
     rhs = np.zeros(network.nr_p)
     rhs[0] = 1e-10
     rhs[100] = -1e-10
-    sol = solve_with_msrsb(A.indices, A.indptr, A.data, rhs)
+    sol, _, _ = solve_with_msrsb(A.indices, A.indptr, A.data, rhs, tol=1e-12)
 
     A[1, :] = 0.0
     A[1, 1] = 1.0
-    sol_exact = solve_pyamg(A, rhs, tol=1e-10)
+    sol_exact = spsolve(A=A, b=rhs)
 
-    sol = (sol - np.min(sol))
-    sol_exact = (sol_exact-np.min(sol_exact))
+    sol -= np.min(sol)
+    sol_exact -= np.min(sol_exact)
     error = sol - sol_exact
 
     Linf = np.linalg.norm(error, ord=np.inf) / np.linalg.norm(sol_exact, ord=np.inf)
