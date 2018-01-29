c86bd9898e0e0e9bb1c837dd54f3346a3413fda7
kkhayrat <khayratk@ifd.mavt.ethz.ch>
2018-01-29 18:38:18 +0100

    example for compressible problem matrix updated

--- pypnm/ams/msrb_funcs.py
+++ pypnm/ams/msrb_funcs.py
@@ -1,7 +1,6 @@
 import pymetis
 
 from PyTrilinos import Epetra
-from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import connected_components
 import numpy as np
 
@@ -11,15 +10,8 @@ from pypnm.linalg.trilinos_interface import matrix_scipy_to_epetra, vector_numpy
 from pypnm.util.igraph_utils import scipy_matrix_to_igraph, coarse_graph_from_partition, support_of_basis_function
 
 from pypnm.multiscale.multiscale_unstructured import MultiScaleSimUnstructured
-
-
-def solve_multiscale(ms, A, b, tol=1e-5):
-    ms.A = A
-    ms.smooth_prolongation_operator(100)
-    sol = Epetra.Vector(A.RangeMap())
-    sol = ms.iterative_solve(b, sol, tol=tol, max_iter=5000)
-    return sol
-
+from scipy.sparse import dia_matrix
+from scipy.sparse.linalg import norm
 
 def partition_multicomponent_graph(A_scipy, v_per_subdomain=1000):
     final_labels = -np.ones(A_scipy.shape[0], dtype=np.int)
@@ -42,131 +34,42 @@ def partition_multicomponent_graph(A_scipy, v_per_subdomain=1000):
     return partition_ind_shift, final_labels
 
 
-def solve_with_msrsb_compressible(ia, ja, a, b, tol=1e-5, v_per_subdomain=1000, cut_off=200, return_inds=False):
-    A = csr_matrix((a, ja, ia))
-    from scipy.sparse import dia_matrix
-    from collections import Counter
+def solve_with_msrsb(A_scipy, rhs, tol=1e-5, v_per_subdomain=1000, n_smooth=10, smoother="ilu", with_multiscale=True,
+                     conv_history=False, max_iter=200, tol_basis=1e-2, adapt_smoothing=True, verbose=False):
 
-    diagonal = dia_matrix(((A + A.T) * np.ones(A.shape[0]), [0]), shape=A.shape)
-    A_b = 0.5 * (A + A.T - diagonal)
+    A_epetra = matrix_scipy_to_epetra(A_scipy)
 
-    num_components, v2components = connected_components(A_b)
-    component_sizes = Counter(v2components)
-    large_components = {component for component in component_sizes if component_sizes[component] > cut_off}
+    try:
+        max_diff = np.max((A_scipy - A_scipy.T).data)
+    except ValueError:
+        max_diff = 0
 
-    inds_in_large_components = list()
-    inds_in_small_components = list()
-    for v, label in enumerate(v2components):
-        if label in large_components:
-            inds_in_large_components.append(v)
-        else:
-            inds_in_small_components.append(v)
-
-    inds_in_large_components = np.asarray(inds_in_large_components)
-    inds_in_small_components = np.asarray(inds_in_small_components)
-
-    A_trunc = A[:, inds_in_large_components][inds_in_large_components, :]
-    A_trunc_b = A_b[:, inds_in_large_components][inds_in_large_components, :]
-
-    my_restriction_supports, my_basis_support = get_supports(A_trunc_b, v_per_subdomain)
-
-    A_b_trunc_epetra = matrix_scipy_to_epetra(A_trunc_b)
-    A_trunc_epetra = matrix_scipy_to_epetra(A_trunc)
-
-    ms = MSRSB(A_b_trunc_epetra, my_restriction_supports, my_basis_support)
-
-    ms.smooth_prolongation_operator(niter=100)
-
-    sol_trunc = Epetra.Vector(ms.A.RangeMap())
-    rhs_trunc = Epetra.Vector(ms.A.RangeMap())
-
-    rhs_trunc[:] = b[inds_in_large_components]
-    sol_trunc[:] = 0.0
-
-    sol_trunc = ms.iterative_solve(rhs_trunc, sol_trunc, tol=tol, max_iter=100, A=A_trunc_epetra)
-    sol = np.zeros_like(b)
-    sol[inds_in_large_components] = sol_trunc
-
-    if return_inds:
-        return sol, inds_in_large_components
-
-    return sol
-
-
-def solve_with_msrsb(ia, ja, a, rhs, tol=1e-3, v_per_subdomain=1000):
-    comm = Epetra.PyComm()
-    num_proc = comm.NumProc()
-
-    A_scipy = csr_matrix((a, ia, ja))
-
-    graph = scipy_matrix_to_igraph(A_scipy)
-
-    A = matrix_scipy_to_epetra(A_scipy)
-
-    num_subdomains = A_scipy.get_shape()[0] / v_per_subdomain
-
-    print "num of subdomains created", num_subdomains
-
-    # create global_id attributes before creating subgraphs.
-    graph.vs["global_id"] = np.arange(graph.vcount())
-    graph.es["global_id"] = np.arange(graph.ecount())
-
-    n_components, _ = connected_components(A_scipy, directed=False)
-
-    if n_components == 1:
-        _, subgraph_ids_each_vertex = pymetis.part_graph(num_subdomains, graph.get_adjlist())
+    if max_diff < norm(A_scipy)*1e-10:
+        print "matrix is symmetric"
+        A_b_scipy = A_scipy
+        A_b_epetra = A_epetra
     else:
-        _, subgraph_ids_each_vertex = partition_multicomponent_graph(A_scipy, v_per_subdomain)
-        print "num of subdomains created", _
+        print "matrix is non symmetric"
+        diagonal = dia_matrix(((A_scipy + A_scipy.T) * np.ones(A_scipy.shape[0]), [0]), shape=A_scipy.shape)
+        A_b_scipy = 0.5 * (A_scipy + A_scipy.T - diagonal)
+        A_b_epetra = matrix_scipy_to_epetra(A_b_scipy)
 
-    subgraph_ids_each_vertex = np.asarray(subgraph_ids_each_vertex)
-    graph.vs["subgraph_id"] = subgraph_ids_each_vertex
+    my_restriction_supports, my_basis_support = get_supports(A_b_scipy, v_per_subdomain)
 
-    # Assign a processor id to each subgraph
-    coarse_graph = coarse_graph_from_partition(graph, subgraph_ids_each_vertex)
+    ms = MSRSB(A_b_epetra, my_restriction_supports, my_basis_support)
 
-    _, proc_ids = pymetis.part_graph(num_proc, coarse_graph.get_adjlist())
-    coarse_graph.vs['proc_id'] = proc_ids
-    coarse_graph.vs["subgraph_id"] = np.arange(coarse_graph.vcount())
+    ms.smooth_prolongation_operator(A_b_epetra, max_iter=10000, tol=tol_basis)
 
-    # Assign a processor id to each pore
-    subgraph_id_to_proc_id = {v["subgraph_id"]: v['proc_id'] for v in coarse_graph.vs}
-    graph.vs["proc_id"] = [subgraph_id_to_proc_id[v["subgraph_id"]] for v in graph.vs]
+    sol = Epetra.Vector(A_epetra.RangeMap())
+    sol, history = ms.iterative_solve(A_epetra, vector_numpy_to_epetra(rhs), sol, tol=tol, max_iter=max_iter,
+                                      n_smooth=n_smooth, smoother=smoother, with_multiscale=with_multiscale,
+                                      conv_history=True, adapt_smoothing=adapt_smoothing, verbose=verbose)
 
-    subgraph_ids = np.unique(subgraph_ids_each_vertex)
-
-    subgraph_id_to_v_center_id = MultiScaleSimUnstructured.subgraph_central_vertices(graph, subgraph_ids)
-
-    # Epetra maps to facilitate data transfer between processors
-    unique_map, nonunique_map, subgraph_ids_vec = MultiScaleSimUnstructured.create_maps(graph, comm)
-
-    epetra_importer = Epetra.Import(nonunique_map, unique_map)
-    assert epetra_importer.NumPermuteIDs() == 0
-    assert epetra_importer.NumSameIDs() == unique_map.NumMyElements()
-
-    my_basis_support = dict()
-
-    my_global_elements = unique_map.MyGlobalElements()
-
-    graph["global_to_local"] = dict((v["global_id"], v.index) for v in graph.vs)
-    graph["local_to_global"] = dict((v.index, v["global_id"]) for v in graph.vs)
-
-    my_restriction_supports = dict()
-
-    subgraph_id_vec = np.asarray(graph.vs['subgraph_id'])
-    for i in subgraph_ids:
-        vs_subgraph = (subgraph_id_vec == i).nonzero()[0]
-        my_restriction_supports[i] = graph.vs.select(vs_subgraph)["global_id"]
-
-    for i in subgraph_ids:
-        support_vertices = support_of_basis_function(i, graph, coarse_graph, subgraph_id_to_v_center_id,
-                                                     my_restriction_supports)
-
-        my_basis_support[i] = np.intersect1d(support_vertices, my_global_elements).astype(np.int32)
-
-    ms = MSRSB(A, my_restriction_supports, my_basis_support)
-    sol = np.asarray(solve_multiscale(ms, A, vector_numpy_to_epetra(rhs), tol=tol))
-    return sol, ms, subgraph_id_to_v_center_id
+    sol = np.asarray(sol)
+    if conv_history:
+        return sol, history
+    else:
+        return sol
 
 
 def get_supports(A_scipy, v_per_subdomain=1000):
