af3233ab98444eb6a0d6de957c1cfa51b67eb901
kkhayrat <khayratk@ifd.mavt.ethz.ch>
2018-01-29 19:01:48 +0100

    relperm interface change

--- pypnm/porenetwork/relperm.py
+++ pypnm/porenetwork/relperm.py
@@ -5,11 +5,17 @@ from pypnm.attribute_calculators.conductance_calc import ConductanceCalc
 from pypnm.percolation import graph_algs
 from pypnm.porenetwork.constants import *
 from pypnm.linalg.petsc_interface import petsc_solve
+from functools import reduce
 
 
 class SimpleRelPermComputer(object):
     def __init__(self, network, fluid_properties, pores_have_conductance=False):
         self.network = network
+        x_max, x_min = np.max(network.pores.x), np.min(network.pores.x)
+        y_max, y_min = np.max(network.pores.y), np.min(network.pores.y)
+        z_max, z_min = np.max(network.pores.z), np.min(network.pores.z)
+        self.network_vol = (x_max - x_min) * (y_max - y_min) * (z_max - z_min)
+
         self.total_flux_one_phase = None
         self.kr_n = np.zeros(3)
         self.kr_w = np.zeros(3)
@@ -24,22 +30,6 @@ class SimpleRelPermComputer(object):
 
         self.cond_computer = ConductanceCalc(self.network, fluid_properties, pores_have_conductance)
         self.compute_absolute_permeability()
-
-    def compute_isotropic(self):
-        kr_n, kr_w = self.compute_relperms(self.ls, self.network.pi_list_face[WEST], self.network.pi_list_face[EAST])
-        self.kr_n[:] = kr_n
-        self.kr_w[:] = kr_w
-
-        assert (kr_n <= 1.0001), kr_n
-        assert (kr_w <= 1.0001), kr_w
-
-    def compute_anisotropic(self):
-        self.kr_n[0], self.kr_w[0] = self.compute_relperms(self.ls, self.network.pi_list_face[WEST], self.network.pi_list_face[EAST])
-        self.kr_n[1], self.kr_w[1] = self.compute_relperms(self.ls, self.network.pi_list_face[SOUTH], self.network.pi_list_face[NORTH])
-        self.kr_n[2], self.kr_w[2] = self.compute_relperms(self.ls, self.network.pi_list_face[BOTTOM], self.network.pi_list_face[TOP])
-
-        assert np.all(self.kr_n <= 18.0)
-        assert np.all(self.kr_w <= 18.0)
 
 
     def _general_absolute_permeability(self, coord, pi_inlet, pi_outlet):
@@ -56,10 +46,53 @@ class SimpleRelPermComputer(object):
         mu_w = self.fluid_properties["mu_w"]
         self.total_flux_one_phase = flux_into_pores(network, pressure, tube_k_w, pi_inlet)
 
-        x_max = np.mean(coord[pi_outlet])
-        x_min = np.mean(coord[pi_inlet])
+        coord_max = np.min(coord)
+        coord_min = np.max(coord)
 
-        return mu_w * np.abs(self.total_flux_one_phase) * (x_max-x_min)
+        return mu_w * np.abs(self.total_flux_one_phase) * (coord_max-coord_min)**2/ self.network_vol
+
+    def _general_effective_nonwetting_permeability(self, coord, pi_inlet, pi_outlet):
+        network = self.network
+        tube_k_n, tube_k_w = self.cond_computer.conductances_two_phase()
+
+        p_conn_inlet, t_conn_inlet = graph_algs.get_pore_and_tube_nonwetting_connectivity_to_pore_list(network, pi_inlet)
+        is_nwett_pore_outlet_conn_to_inlet = np.any(p_conn_inlet[pi_outlet] == 1)
+
+        if not is_nwett_pore_outlet_conn_to_inlet:
+            kr_n_eff = 0.0
+        else:
+            pi_dirichlet = reduce(np.union1d, (pi_inlet, pi_outlet, (p_conn_inlet == 0).nonzero()[0]))
+            A = laplacian_from_network(network, weights=tube_k_n, ind_dirichlet=pi_dirichlet)
+
+            rhs = np.zeros(network.nr_p)
+            rhs[pi_inlet] = 1.0
+
+            pressure = petsc_solve(A * 1e20, rhs * 1e20, tol=1e-12)
+
+            mu_n = self.fluid_properties["mu_n"]
+            total_flux_nwett = flux_into_pores(network, pressure, tube_k_n, pi_inlet)
+
+            coord_max = np.min(coord)
+            coord_min = np.max(coord)
+
+            kr_n_eff = mu_n * np.abs(total_flux_nwett) * (coord_max-coord_min)**2 / self.network_vol
+
+        return kr_n_eff
+
+    def effective_nonwetting_permeability(self):
+        network = self.network
+        K_n = [0]*3
+
+        K_n[0] = self._general_effective_nonwetting_permeability(network.pores.x,
+                                                        network.pi_list_face[WEST], network.pi_list_face[EAST])
+
+        K_n[1] = self._general_effective_nonwetting_permeability(network.pores.y,
+                                                        network.pi_list_face[SOUTH], network.pi_list_face[NORTH])
+
+        K_n[2] = self._general_effective_nonwetting_permeability(network.pores.z,
+                                                        network.pi_list_face[BOTTOM], network.pi_list_face[TOP])
+
+        return K_n
 
     def compute_absolute_permeability(self):
         network = self.network
@@ -74,47 +107,3 @@ class SimpleRelPermComputer(object):
                                                         network.pi_list_face[BOTTOM], network.pi_list_face[TOP])
 
         assert np.all(self.K > 0.0), self.K
-
-    def compute_relperms(self, ls, pi_in, pi_out):
-        network = self.network
-        self.cond_computer.compute()
-
-        ls.solve_pressure(WETT, pi_in, pi_out, self.mask_ignored_tubes)
-        total_flux_wett = flux_into_pores(self.network, ls.sol, self.network.tubes.k_w, pi_in)
-        kr_w = total_flux_wett / self.total_flux_one_phase
-
-        is_nwett_pore_inlet = np.any(network.pores.invaded[pi_in] == 1)
-        is_nwett_pore_outlet = np.any(network.pores.invaded[pi_out] == 1)
-
-        if not (is_nwett_pore_inlet and is_nwett_pore_outlet):
-            kr_n = 0.0
-        else:
-            p_conn_inlet, t_conn_inlet = graph_algs.get_pore_and_tube_nonwetting_connectivity_to_pore_list(network, pi_in)
-            is_nwett_pore_outlet_conn_to_inlet = np.any(p_conn_inlet[pi_out] == 1)
-            if not is_nwett_pore_outlet_conn_to_inlet:
-                kr_n = 0.0
-            else:
-                ls.solve_pressure(NWETT, pi_in, pi_out, t_conn_inlet==0)
-
-                total_flux_nwett = flux_into_pores(self.network, ls.sol, self.network.tubes.k_n, pi_in)
-
-                mu_n = self.fluid_properties['mu_n']
-                mu_w = self.fluid_properties['mu_w']
-                kr_n = total_flux_nwett / self.total_flux_one_phase * mu_n / mu_w
-
-        if kr_n < -10.0e-9:
-            print kr_n
-
-        eps = 1.e-3
-
-        assert (kr_n >= 0.0), "kr_n:%g kr_w:%g"%(kr_n, kr_w)
-        assert (kr_w >= 0.0), "kr_n:%g kr_w:%g"%(kr_n, kr_w)
-        assert (kr_w <= 1.0+eps), "kr_n:%e kr_w:%e"%(kr_n, kr_w)
-        assert (kr_n <= 1.0+eps), "kr_n:%e kr_w:%e"%(kr_n, kr_w)
-        kr_n = abs(kr_n)
-        kr_w = abs(kr_w)
-
-        return kr_n, kr_w
-
-    def compute(self):
-        self.compute_isotropic()
