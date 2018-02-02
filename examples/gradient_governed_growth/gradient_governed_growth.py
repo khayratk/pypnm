f3a3c0e3c38685e3d4736501307c9bceafadda79
kkhayrat <khayratk@ifd.mavt.ethz.ch>
2018-02-02 18:18:48 +0100

    Update algorithm for speed

--- examples/gradient_governed_growth/gradient_governed_growth.py
+++ examples/gradient_governed_growth/gradient_governed_growth.py
@@ -2,6 +2,13 @@
 Implementation of gradient governed percolation from scratch using pypnm
 """
 
+import sys
+import petsc4py
+from mpi4py import MPI
+
+petsc4py.init(sys.argv)
+from petsc4py import PETSc
+
 import cProfile
 import pstats
 
@@ -9,10 +16,10 @@ import numpy as np
 from numpy.random import choice
 
 from pypnm.linalg.laplacianmatrix import laplacian_from_network
-from pypnm.linalg.petsc_interface import petsc_solve
+from pypnm.linalg.petsc_interface import  scipy_to_petsc_matrix
 from pypnm.porenetwork.constants import WEST, EAST
 from pypnm.porenetwork.network_factory import unstructured_network_delaunay
-
+from pypnm.porenetwork.porenetwork import PoreNetwork
 
 def get_interface_invasion_throats(network, pressure, entry_pressure, tube_conductances):
     pore_list_1 = network.edgelist[:, 0]
@@ -41,13 +48,25 @@ def get_interface_invasion_throats(network, pressure, entry_pressure, tube_condu
 
 
 def run():
-    network = unstructured_network_delaunay(100000, quasi_2d=True)
+    comm = MPI.COMM_SELF
+
+    try:
+        network = PoreNetwork.load("benchmark_network.pkl")
+
+    except IOError:
+        network = unstructured_network_delaunay(200000, quasi_2d=True)
+        network.save("benchmark_network.pkl")
+
     mu_w = 1.0
-    mu_n = 0.1
+    mu_n = 0.01
     gamma = 1.0
     network.pores.invaded[network.pi_list_face[WEST]] = 1
     pressure = np.zeros(network.nr_p)
     entry_pressure = 2 * gamma / network.tubes.r
+
+    ds_out = 0.01
+    s_target = 0.01
+    n_out = 0
 
     sf = 1.0e20
 
@@ -57,10 +76,32 @@ def run():
     source = np.zeros(network.nr_p)
     source[network.pi_list_face[WEST]] = 1.e-9
     outlet_pore_set = set(network.pi_list_face[EAST])
-    try:
 
-        for niter in xrange(100000):
-            pressure = petsc_solve(A * sf, source * sf, x0=pressure, ksptype="minres", tol=1e-10)
+    ksp = PETSc.KSP().create(comm=comm)
+    ksp.setTolerances(rtol=1e-8, max_it=10000)
+    ksp.setType("minres")
+    pc = ksp.getPC()
+    pc.setType("gamg")
+    ksp.setInitialGuessNonzero(True)
+    ksp.setFromOptions()
+    petsc_mat = scipy_to_petsc_matrix(A * sf)
+    ksp.setOperators(A=petsc_mat)
+
+    change_list = []
+    try:
+        for niter in xrange(4000000):
+            petsc_mat = scipy_to_petsc_matrix(A*sf)
+            _, P = ksp.getOperators()
+
+            if niter %10 == 0:
+                P = petsc_mat
+
+            ksp.setOperators(A=petsc_mat, P=P)
+            petsc_rhs = PETSc.Vec().createWithArray(source * sf, comm=comm)
+            petsc_sol = PETSc.Vec().createWithArray(pressure, comm=comm)
+            ksp.solve(petsc_rhs, petsc_sol)
+
+            pressure = petsc_sol.getArray()
 
             displacing_tubes, prob = get_interface_invasion_throats(network, pressure, entry_pressure,
                                                                     tube_conductances)
@@ -72,6 +113,8 @@ def run():
             # Update CSR matrix
             g_12_new = np.pi * (network.tubes.r[ti_displaced] ** 4) / (8. * network.tubes.l[ti_displaced] * mu_n)
             p1, p2 = network.edgelist[ti_displaced]
+            change_list.append(g_12_new + A[p1, p2])
+
             if p1 not in outlet_pore_set:
                 A[p1, p1], A[p1, p2] = A[p1, p1] + A[p1, p2] + g_12_new, -g_12_new
             if p2 not in outlet_pore_set:
@@ -79,12 +122,20 @@ def run():
 
             tube_conductances[ti_displaced] = g_12_new
 
-            if niter % 10000 == 0:
+            saturation = np.sum(network.tubes.vol[network.tubes.invaded == 1]) / network.total_vol + np.sum(
+                network.pores.vol[network.pores.invaded == 1]) / network.total_vol
+
+            if saturation > s_target:
                 network.pores.p_n[:] = pressure
-                #network.export_to_vtk("output_viscous" + str(niter).zfill(4))
+                print "Number of throats invaded:", niter
+                network.save("network_history/network"+str(n_out).zfill(5)+".pkl")
+                s_target += ds_out
+                n_out += 1
+
+            if niter % 10 == 0:
 
                 print "Number of throats invaded:", niter
-                network.save("network_history/network"+str(niter).zfill(7)+".pkl")
+                print "saturation is:", saturation
 
     except KeyboardInterrupt:
         pass
